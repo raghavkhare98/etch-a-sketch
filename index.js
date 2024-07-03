9765203cdf2daaeb92da519656f2e2ea3d2bc5a6
raghavkhare98 <khare08rma@gmail.com>
2024-07-03 12:52:19 -0400

    Add style, font, and create grids

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const containerDiv = document.querySelector(".container");
-for(let i=0; i<17; i++){
+for(let i=0; i<16; i++){
     girdDiv = document.createElement("div");
+    girdDiv.classList.add("grid-box");
     containerDiv.appendChild(girdDiv);
 }
 
